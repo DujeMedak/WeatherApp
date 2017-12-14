29ceaa77b68740898cce846e10756558adead9e5
DustyBones <tiago.fa.figueiredo@gmail.com>
2017-12-14 13:28:37 +0000

    skia

--- WeatherApp/WeatherApp/Views/HistoryWeatherPage.xaml.cs
+++ WeatherApp/WeatherApp/Views/HistoryWeatherPage.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using SkiaSharp;
+using SkiaSharp.Views.Forms;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -31,6 +33,28 @@ namespace WeatherApp.Views
             ws = new WeatherServices();
 
             BindingContext = this.viewModel = vm;
+        }
+
+        void OnCanvasViewPaintSurface(object sender, SKPaintSurfaceEventArgs args)
+        {
+            //Initialize the Canvas  
+
+            SKImageInfo info = args.Info;
+            SKSurface surface = args.Surface;
+            SKCanvas canvas = surface.Canvas;
+            //clear
+            canvas.Clear();
+
+
+            SKPaint paint = new SKPaint
+            {
+                Style = SKPaintStyle.Fill,
+                Color = Color.Red.ToSKColor(),
+                StrokeWidth = 50
+            };
+
+            //canvas.DrawRect(new SKRect(200, 200, 200, 200), paint);
+            canvas.DrawCircle(args.Info.Width / 2, args.Info.Height / 2, 100, paint);
         }
 
     }
